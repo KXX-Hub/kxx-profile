e5102bda07cf23a032738b843dcc66c4bb681a38
KXX-HUB <97007ken@gmail.com>
2024-10-11 21:02:21 +0800

    Update - using react to generate website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonalPage from './PersonalPage';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalPage />
+    </div>
+  );
+}
+
+export default App;
