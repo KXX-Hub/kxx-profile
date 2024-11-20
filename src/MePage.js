f30c3ae8040c4644ee7ca5f0f2db62d7047eaff9
KXX-HUB <97007ken@gmail.com>
2024-11-20 11:33:41 +0800

    Update - finish MePage

--- src/MePage.js
+++ src/MePage.js
@@ -3,16 +3,28 @@ import { Link } from 'react-router-dom';
 import './MePage.css'; // Import the MePage styles
 
 const MePage = () => {
-  const content = `
-About Me:
-  - Software Developer
-  - Cybersecurity Engineer Intern
-  - IEEE author`;
+  const linkedinUrl = 'https://www.linkedin.com/in/kai-hung-0xkxx/'; // Replace with your GitHub URL
+  const githubUrl = 'https://github.com/KXX-Hub'; // Replace with your LinkedIn URL
 
   return (
     <div className="me-page">
       <h1>About Me</h1>
-      <pre className="coding-content">{content}</pre>
+      <div className="profile-links">
+        <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+          <img
+            src="img/Github.png"
+            alt="GitHub"
+            className="icon"
+          />
+        </a>
+        <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+          <img
+            src="img/LinkedIn.png"
+            alt="LinkedIn"
+            className="icon"
+          />
+        </a>
+      </div>
       <Link to="/" className="back-home-btn">Back to Home</Link>
     </div>
   );
